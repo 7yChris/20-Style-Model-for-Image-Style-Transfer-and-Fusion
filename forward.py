f703979d7505e0a82fe8089405d35ebcd8c6627a
7yChris <chrispaul_qy@126.com>
2018-12-24 09:31:15 +0800

    Add files via upload

--- forward.py
+++ forward.py
@@ -1,30 +1,30 @@
 # -*- coding: UTF-8 -*-
-import numpy as np
-from ops import *
-
+import numpy as np    #导入numpy模块
+from ops import *     #导入ops所有模块
+#卷积
 def conv_(inputs, w, b):
-    return tf.nn.conv2d(inputs, w, [1, 1, 1, 1], "SAME") + b
-
+    return tf.nn.conv2d(inputs, w, [1, 1, 1, 1], "SAME") + b      #卷积函数，inputs：输入，卷积核，步长，前后形状相同
+#池化
 def max_pooling(inputs):
-    return tf.nn.max_pool(inputs, [1, 2, 2, 1], [1, 2, 2, 1], "SAME")
-
+    return tf.nn.max_pool(inputs, [1, 2, 2, 1], [1, 2, 2, 1], "SAME")   #池化函数：输入，池化窗口大小，滑动步长，前后形状
+#vgg16提取卷积网络特征
 def vggnet(inputs, vgg_path):
-    inputs = tf.reverse(inputs, [-1]) - np.array([103.939, 116.779, 123.68])
-    para = np.load(vgg_path+"vgg16.npy", encoding="latin1").item()
-    F = {}
-    inputs = relu(conv_(inputs, para["conv1_1"][0], para["conv1_1"][1]))
-    inputs = relu(conv_(inputs, para["conv1_2"][0], para["conv1_2"][1]))
+    inputs = tf.reverse(inputs, [-1]) - np.array([103.939, 116.779, 123.68])     #处理输入图片
+    para = np.load(vgg_path+"vgg16.npy", encoding="latin1").item()               #从vgg模型路径获得vgg模型
+    F = {}                                                                       #定义返回值为字典F
+    inputs = relu(conv_(inputs, para["conv1_1"][0], para["conv1_1"][1]))         #使用vgg中的conv1_1,conv1_2完成卷积
+    inputs = relu(conv_(inputs, para["conv1_2"][0], para["conv1_2"][1]))         #激活函数 relu，即将矩阵中每行的非最大值置0。
     F["conv1_2"] = inputs
-    inputs = max_pooling(inputs)
-    inputs = relu(conv_(inputs, para["conv2_1"][0], para["conv2_1"][1]))
+    inputs = max_pooling(inputs)                                                 #池化
+    inputs = relu(conv_(inputs, para["conv2_1"][0], para["conv2_1"][1]))         #第二层卷积提取特征
     inputs = relu(conv_(inputs, para["conv2_2"][0], para["conv2_2"][1]))
     F["conv2_2"] = inputs
-    inputs = max_pooling(inputs)
-    inputs = relu(conv_(inputs, para["conv3_1"][0], para["conv3_1"][1]))
+    inputs = max_pooling(inputs)                                                 #池化
+    inputs = relu(conv_(inputs, para["conv3_1"][0], para["conv3_1"][1]))         #第三层卷积提取特征
     inputs = relu(conv_(inputs, para["conv3_2"][0], para["conv3_2"][1]))
     inputs = relu(conv_(inputs, para["conv3_3"][0], para["conv3_3"][1]))
     F["conv3_3"] = inputs
-    inputs = max_pooling(inputs)
+    inputs = max_pooling(inputs)                                                 #池化+第四层卷积提取特征
     inputs = relu(conv_(inputs, para["conv4_1"][0], para["conv4_1"][1]))
     inputs = relu(conv_(inputs, para["conv4_2"][0], para["conv4_2"][1]))
     inputs = relu(conv_(inputs, para["conv4_3"][0], para["conv4_3"][1]))
@@ -32,7 +32,7 @@ def vggnet(inputs, vgg_path):
     return F
 
 def forward(inputs, y1, y2, alpha):
-    inputs = tf.reverse(inputs, [-1]) - np.array([103.939, 116.779, 123.68])
+    inputs = tf.reverse(inputs, [-1]) - np.array([103.939, 116.779, 123.68])   #处理输入图片
     # 3层卷积
     inputs = relu(conditional_instance_norm(conv("conv1", inputs, 9, 3, 32, 1), "cin1", y1, y2, alpha))
     inputs = relu(conditional_instance_norm(conv("conv2", inputs, 3, 32, 64, 2), "cin2", y1, y2, alpha))
@@ -43,10 +43,8 @@ def forward(inputs, y1, y2, alpha):
     inputs = ResBlock("res3", inputs, 3, 128, 128, y1, y2, alpha)
     inputs = ResBlock("res4", inputs, 3, 128, 128, y1, y2, alpha)
     inputs = ResBlock("res5", inputs, 3, 128, 128, y1, y2, alpha)
-    #
     inputs = upsampling("up1", inputs, 128, 64, y1, y2, alpha)
     inputs = upsampling("up2", inputs, 64, 32, y1, y2, alpha)
-    #
     inputs = sigmoid(conditional_instance_norm(conv("last", inputs, 9, 32, 3, 1), "cinout", y1, y2, alpha)) * 255
     return inputs
 
--- test.py
+++ test.py
@@ -1,67 +1,58 @@
 # -*- coding: UTF-8 -*-
-import tensorflow as tf
-import numpy as np
-from PIL import Image
-from forward import forward
-import argparse
+import tensorflow as tf               #导入tensorflow模块
+import numpy as np                    #导入numpy模块
+from PIL import Image                 #导入PIL模块
+from forward import forward           #导入前向网络模块
+import argparse                       #导入参数选择模块
 
 # 设置参数
-parser = argparse.ArgumentParser()
-parser.add_argument("--C_NUMS", type=int, default=10)
-parser.add_argument("--PATH_MODEL", type=str, default="./save_para/")
-parser.add_argument("--PATH_RESULTS", type=str, default="./results/")
-# 选择测试图像
-parser.add_argument("--PATH_IMG", type=str, default="./imgs/5.jpg")
-# 选择两种风格1
-parser.add_argument("--LABEL_1", type=int, default=9)
-# 选择风格2
-parser.add_argument("--LABEL_2", type=int, default=4)
-# Alpha，默认为0.5
-parser.add_argument("--ALPHA", type=float, default=0.5)
-args = parser.parse_args()
+parser = argparse.ArgumentParser()    #定义一个参数设置器
+parser.add_argument("--C_NUMS", type=int, default=10)     #参数：图片数量，默认值为10
+parser.add_argument("--PATH_MODEL", type=str, default="./save_para/")    #参数：模型存储路径
+parser.add_argument("--PATH_RESULTS", type=str, default="./results/")    #参数：结果存储路径
+parser.add_argument("--PATH_IMG", type=str, default="./imgs/5.jpg")      #参数：选择测试图像
+parser.add_argument("--LABEL_1", type=int, default=9)                    #参数：风格1
+parser.add_argument("--LABEL_2", type=int, default=4)                    #参数：风格2
+parser.add_argument("--ALPHA", type=float, default=0.5)                  #参数：Alpah，风格权重，默认为0.5
+args = parser.parse_args()                                               #定义参数集合args
 
 
-def Init(c_nums=10, model_path=args.PATH_MODEL):
-    content = tf.placeholder(tf.float32, [1, None, None, 3])
-    y1 = tf.placeholder(tf.float32, [1, c_nums])
-    y2 = tf.placeholder(tf.float32, [1, c_nums])
-    alpha = tf.placeholder(tf.float32)
-    target = forward(content, y1, y2, alpha)
-    sess = tf.Session()
-    sess.run(tf.global_variables_initializer())
-    saver = tf.train.Saver()
-    ckpt = tf.train.get_checkpoint_state(model_path)
-    if ckpt and ckpt.model_checkpoint_path:
-        # 从检查点中恢复模型
-        saver.restore(sess, ckpt.model_checkpoint_path)
-        # 从检查点的路径名中分离出训练轮数
-        global_step = ckpt.model_checkpoint_path.split('/')[-1].split('-')[-1]
-    return target, sess, content, y1, y2, alpha
+def Init(c_nums=10, model_path=args.PATH_MODEL):                         #初始化图片生成模型参数
+    content = tf.placeholder(tf.float32, [1, None, None, 3])             #图片输入定义
+    y1 = tf.placeholder(tf.float32, [1, c_nums])                         #初始化风格1选择范围
+    y2 = tf.placeholder(tf.float32, [1, c_nums])                         #初始化风格2选择范围
+    alpha = tf.placeholder(tf.float32)                                   #风格与内容的权重比
+    target = forward(content, y1, y2, alpha)                             #定义将要生成的图片
+    sess = tf.Session()                                                  #定义一个sess
+    sess.run(tf.global_variables_initializer())                          #模型初始化
+    saver = tf.train.Saver()                                             #模型存储器定义
+    ckpt = tf.train.get_checkpoint_state(model_path)                     #从模型存储路径中获取模型
+    if ckpt and ckpt.model_checkpoint_path:                              #从检查点中恢复模型
+        saver.restore(sess, ckpt.model_checkpoint_path)                  #从检查点的路径名中分离出训练轮数
+        global_step = ckpt.model_checkpoint_path.split('/')[-1].split('-')[-1]   #获取训练步数
+    return target, sess, content, y1, y2, alpha                          #返回目标图片，模型session，输入图片，风格选择，风格权重
 
 
-def stylize(img_path, result_path, label1, label2, alpha, target, sess, content_ph, y1_ph, y2_ph, alpha_ph):
-    img = np.array(Image.open(img_path))
-    Y1 = np.zeros([1, 10])
-    Y2 = np.zeros([1, 10])
-    Y1[0, label1] = 1
-    Y2[0, label2] = 1
-    img = sess.run(target, feed_dict={content_ph: img[np.newaxis, :, :, :], y1_ph: Y1, y2_ph: Y2, alpha_ph: alpha})
-    print(alpha)
-    Image.fromarray(np.uint8(img[0, :, :, :])).save(result_path + "test" + str(alpha) + ".jpg")
+def stylize(img_path, result_path, label1, label2, alpha, target, sess, content_ph, y1_ph, y2_ph, alpha_ph): # 风格迁移
+    img = np.array(Image.open(img_path))               #将输入图片序列化
+    Y1 = np.zeros([1, 10])                             #数组置0
+    Y2 = np.zeros([1, 10])                             #数组置0
+    Y1[0, label1] = 1                                  #第label1个风格置1
+    Y2[0, label2] = 1                                  #第label2个风格置1
+    img = sess.run(target, feed_dict={content_ph: img[np.newaxis, :, :, :], y1_ph: Y1, y2_ph: Y2, alpha_ph: alpha})  #生成图片
+    Image.fromarray(np.uint8(img[0, :, :, :])).save(result_path + "result" + str(alpha) + ".jpg")                   #保存风格迁移后的图片
 
-
+#测试程序
 def test():
-    target, sess, content, y1, y2, alpha = Init(args.C_NUMS, args.PATH_MODEL)
-    for a in range(11):
-        # 2.7需要换成浮点数运行，否则若结果小于1，则为0；python3则不需要，直接除即可
-        args.ALPHA = float(a) / 10.0
-        # 生成融合图像
+    target, sess, content, y1, y2, alpha = Init(args.C_NUMS, args.PATH_MODEL)          #初始化生成模型
+    for a in range(11):                                                                #将风格权重分成10份，计算10种风格不同的图片
+        args.ALPHA = a / 10
         stylize(args.PATH_IMG, args.PATH_RESULTS, args.LABEL_1, args.LABEL_2, args.ALPHA, target, sess, content, y1, y2, alpha)
 
-
+#主程序
 def main():
     test()
 
-
+#主程序入口
 if __name__ == '__main__':
     main()
